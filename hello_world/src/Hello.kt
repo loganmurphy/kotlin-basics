094a678f0a0ec7939d28406584cb25bae451eb9f
Logan Murphy <loganmurphy@Logans-iMac.local>
2020-05-07 23:28:15 -0500

    leanring basics of kotlin functions

---
+++ hello_world/src/Hello.kt
@@ -0,0 +1,48 @@
+import java.util.*
+
+fun isTooHot(temperature: Int) = temperature > 30
+
+fun isDirty(dirty: Int) = dirty > 30
+
+fun isSunday(day: String) = day == "Sunday"
+
+fun feedTheFish() {
+    val day = randomDay()
+    val food = fishFood(day)
+    println("Today is $day and fish eat $food")
+    println("Change water: ${shouldChangeWater(day)}")
+}
+
+fun fishFood(day : String) : String {
+    return when (day) {
+        "Monday" -> "flakes"
+        "Wednesday" -> "redworms"
+        "Thursday" ->  "granules"
+        "Friday" ->  "mosquitos"
+        "Sunday" ->  "plankton"
+        else -> "nothing!"
+    }
+}
+
+fun swim (speed: String = "fast") {
+    println("swimming $speed")
+}
+
+fun shouldChangeWater (day: String, temperature: Int = 22, dirty: Int = 20): Boolean {
+    return when  {
+        isTooHot(temperature) -> true
+        isDirty(dirty) -> true
+        isSunday(day) -> true
+        else -> false
+    }
+}
+
+fun randomDay() : String {
+    val week = arrayOf("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday")
+    return week[Random().nextInt(week.size)]
+}
+
+fun main(args: Array<String>) {
+    feedTheFish()
+}
+
